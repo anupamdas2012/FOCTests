53f2be860003f811712e345332e2db8ee8c68d34
unknown <anupam.das@spectrum-customizer.com>
2025-02-23 10:26:02 -0600

    added platformio workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,67 @@
 #include <SimpleFOC.h>
 
-// BLDC motor & driver instance
+// BLDCMotor(pole pair number, phase resistance (optional) );
 BLDCMotor motor = BLDCMotor(11);
+// BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
 BLDCDriver3PWM driver = BLDCDriver3PWM(4, 5, 6, 0);
-
-// encoder instance
-Encoder encoder = Encoder(17, 16, 2048);
-// channel A and B callbacks
-void doA(){encoder.handleA();}
-void doB(){encoder.handleB();}
 
 // instantiate the commander
 Commander command = Commander(Serial);
-void doMotor(char* cmd) { command.motor(&motor, cmd); }
+void doTarget(char* cmd) { command.scalar(&motor.target, cmd); }
+void doLimit(char* cmd) { command.scalar(&motor.voltage_limit, cmd); }
 
-void setup() { 
-  
+void setup() {
+
   // use monitoring with serial 
   Serial.begin(115200);
   // enable more verbose output for debugging
   // comment out if not needed
   SimpleFOCDebug::enable(&Serial);
-  
-  // initialize encoder sensor hardware
-  encoder.init();
-  encoder.enableInterrupts(doA, doB); 
-  // link the motor to the sensor
-  motor.linkSensor(&encoder);
 
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 12;
-  // driver init
+  driver.voltage_power_supply = 19;
+  // limit the maximal dc voltage the driver can set
+  // as a protection measure for the low-resistance motors
+  // this value is fixed on startup
+  driver.voltage_limit = 6;
   if(!driver.init()){
     Serial.println("Driver init failed!");
     return;
   }
-  // link driver
+  // link the motor and the driver
   motor.linkDriver(&driver);
 
-  // aligning voltage
-  motor.voltage_sensor_align = 5;
+  // limiting motor movements
+  // limit the voltage to be set to the motor
+  // start very low for high resistance motors
+  // current = voltage / resistance, so try to be well under 1Amp
+  motor.voltage_limit = 3;   // [V]
+ 
+  // open loop control config
+  motor.controller = MotionControlType::velocity_openloop;
 
-  // set motion control loop to be used
-  motor.torque_controller = TorqueControlType::voltage;
-  motor.controller = MotionControlType::torque;
-
-  // comment out if not needed
-  motor.useMonitoring(Serial);
-
-  // initialize motor
+  // init motor hardware
   if(!motor.init()){
     Serial.println("Motor init failed!");
     return;
   }
-  // align sensor and start FOC
-  if(!motor.initFOC()){
-    Serial.println("FOC init failed!");
-    return;
-  }
 
-  // set the initial motor target
-  motor.target = 2; // Volts 
+  // set the target velocity [rad/s]
+  motor.target = 6.28; // one rotation per second
 
-  // add target command M
-  command.add('M', doMotor, "Motor");
+  // add target command T
+  command.add('T', doTarget, "target velocity");
+  command.add('L', doLimit, "voltage limit");
 
-  Serial.println(F("Motor ready."));
-  Serial.println(F("Set the target using serial terminal and command M:"));
+  Serial.println("Motor ready!");
+  Serial.println("Set target velocity [rad/s]");
   _delay(1000);
 }
 
 void loop() {
-  // main FOC algorithm function
-  motor.loopFOC();
 
-  // Motion control function
+  // open loop velocity movement
   motor.move();
 
   // user communication
