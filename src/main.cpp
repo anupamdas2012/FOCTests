9ff89c8e3b8be26db9f7c6213663f8fbf6d4dc96
unknown <anupam.das@spectrum-customizer.com>
2025-02-26 01:32:37 -0600

    closed loop voltage test stop at 360

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,107 @@
-// Open loop motor control example
+/**
+ * 
+ * Torque control example using voltage control loop.
+ * 
+ * Most of the low-end BLDC driver boards doesn't have current measurement therefore SimpleFOC offers 
+ * you a way to control motor torque by setting the voltage to the motor instead hte current. 
+ * 
+ * This makes the BLDC motor effectively a DC motor, and you can use it in a same way.
+ */
 #include <SimpleFOC.h>
 
 
 // BLDC motor & driver instance
-// BLDCMotor motor = BLDCMotor(pole pair number);
 BLDCMotor motor = BLDCMotor(11);
-// BLDCDriver3PWM driver = BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
 BLDCDriver3PWM driver = BLDCDriver3PWM(4, 5, 6, 0);
-
 // Stepper motor & driver instance
 //StepperMotor motor = StepperMotor(50);
 //StepperDriver4PWM driver = StepperDriver4PWM(9, 5, 10, 6,  8);
 
+// encoder instance
+Encoder encoder = Encoder(17, 16, 2048);
 
-//target variable
-float target_velocity = 0;
+// Interrupt routine intialisation
+// channel A and B callbacks
+void doA(){encoder.handleA();}
+void doB(){encoder.handleB();}
 
+// voltage set point variable
+float target_voltage = 1;
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
-void doLimit(char* cmd) { command.scalar(&motor.voltage_limit, cmd); }
+void doTarget(char* cmd) { command.scalar(&target_voltage, cmd); }
 
-void setup() {
-
+void setup() { 
+  
   // use monitoring with serial 
   Serial.begin(115200);
   // enable more verbose output for debugging
   // comment out if not needed
   SimpleFOCDebug::enable(&Serial);
 
+  // initialize encoder sensor hardware
+  encoder.init();
+  encoder.enableInterrupts(doA, doB); 
+  // link the motor to the sensor
+  motor.linkSensor(&encoder);
+  motor.monitor_variables = _MON_TARGET | _MON_VEL | _MON_ANGLE; // default _MON_TARGET | _MON_VOLT_Q | _MON_VEL | _MON_ANGLE
+  motor.monitor_downsample = 100; // default 10
+
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 19;
-  // limit the maximal dc voltage the driver can set
-  // as a protection measure for the low-resistance motors
-  // this value is fixed on startup
-  driver.voltage_limit = 6;
-  if(!driver.init()){
-    Serial.println("Driver init failed!");
-    return;
-  }
-  // link the motor and the driver
+  driver.voltage_power_supply = 11;
+  driver.init();
+  // link driver
   motor.linkDriver(&driver);
 
-  // limiting motor movements
-  // limit the voltage to be set to the motor
-  // start very low for high resistance motors
-  // current = voltage / resistance, so try to be well under 1Amp
-  motor.voltage_limit = 3;   // [V]
- 
-  // open loop control config
-  motor.controller = MotionControlType::velocity_openloop;
+  motor.voltage_limit = 2;
+  motor.velocity_limit = 20;
 
-  // init motor hardware
-  if(!motor.init()){
-    Serial.println("Motor init failed!");
-    return;
-  }
+  // aligning voltage
+  motor.voltage_sensor_align = 2;
+  // choose FOC modulation (optional)
+//  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
+
+  // set motion control loop to be used
+  motor.controller = MotionControlType::torque;
+  motor.torque_controller = TorqueControlType::voltage;
+
+  // comment out if not needed
+  motor.useMonitoring(Serial);
+
+  // initialize motor
+  motor.init();
+  // align sensor and start FOC
+  motor.initFOC();
 
   // add target command T
-  command.add('T', doTarget, "target velocity");
-  command.add('L', doLimit, "voltage limit");
+  command.add('T', doTarget, "target voltage");
 
-  Serial.println("Motor ready!");
-  Serial.println("Set target velocity [rad/s]");
+  Serial.println(F("Motor ready."));
+  Serial.println(F("Set the target voltage using serial terminal:"));
   _delay(1000);
 }
 
 void loop() {
 
-  // open loop velocity movement
-  // using motor.voltage_limit and motor.velocity_limit
-  // to turn the motor "backwards", just set a negative target_velocity
-  motor.move(target_velocity);
+  // main FOC algorithm function
+  // the faster you run this function the better
+  // Arduino UNO loop  ~1kHz
+  // Bluepill loop ~10kHz 
+  motor.loopFOC();
 
-  // user communication
+  // Motion control function
+  // velocity, position or voltage (defined in motor.controller)
+  // this function can be run at much lower frequency than loopFOC() function
+  // You can also use motor.move() and set the motor.target in the code
+  motor.move(target_voltage);
+
+  Serial.print(encoder.getAngle());
+
+  if(encoder.getAngle()>6.283)
+  {
+    motor.move(0);
+  }
+  // user communication 
   command.run();
 }
